23eaae6521081e3dc322e1676a67c38947605f01
Artem Zholud <zholud.artem@gmail.com>
2022-04-13 15:18:45 +0200

    правки после теста

--- source/js/modules/according-footer.js
+++ source/js/modules/according-footer.js
@@ -6,6 +6,8 @@ const titleContacts = document.querySelector('.contacts__title');
 const contacts = document.querySelector('.contacts');
 
 function showMenu() {
+  navigation.classList.remove('navigation--no-js');
+
   titleNav.addEventListener('click', () => {
     if (!mainNav.classList.contains('is-open')) {
       mainNav.classList.add('is-open');
@@ -20,6 +22,8 @@ function showMenu() {
 }
 
 function showContacts() {
+  contacts.classList.remove('contacts--no-js');
+
   titleContacts.addEventListener('click', () => {
     if (!contacts.classList.contains('is-open')) {
       contacts.classList.add('is-open');
